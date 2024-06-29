51331b8ab4d96ae001ce96c5719ba869c65c9424
ozanpkr <ozanpeker.deeplearning@gmail.com>
2024-06-29 21:27:55 +0300

    dNBR and NBR have been added to analyzer.

---
+++ analyzer.py
@@ -0,0 +1,135 @@
+import rasterio
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import enum
+from rasterio import plot
+
+class AreaType(enum.Enum):
+    BURNED = 1
+    UNBURNED = 2
+
+class Analyzer:
+    def __init__(self):
+        self.__src = None
+
+    def visualize_ndvi_with_threshold(self, src_dataset, threshold=None, verbose=False):
+        """
+        Rasterio veri kümesinden NDVI haritasını belirli bir eşik değeri üstünde
+        yeşil, diğer alanları kırmızı olarak görselleştirir.
+    
+        Parameters:
+        - src_dataset (rasterio.io.DatasetReader): Rasterio veri kümesi, TIFF dosyasının açılmış hali.
+        - threshold (float): NDVI eşik değeri. Bu değeri geçen alanlar yeşil olacak.
+        - verbose (bool): Görüntüyü görselleştirme modunu etkinleştirir veya devre dışı bırakır.
+    
+        Returns:
+        - ndvi_masked (numpy.ndarray): NDVI haritası (eğer verbose=True ise).
+        """
+        red_band = src_dataset.read(3)
+        nir_band = src_dataset.read(8)
+    
+        np.seterr(divide='ignore', invalid='ignore')
+    
+        ndvi = (nir_band.astype(float) - red_band.astype(float)) / (nir_band + red_band)
+    
+        cmap = 'RdYlGn'
+        if threshold is not None:
+            ndvi_masked = np.zeros_like(ndvi, dtype=np.uint8)
+            ndvi_masked[ndvi >= threshold] = 1
+            ndvi_masked[ndvi < threshold] = 0
+            cmap = LinearSegmentedColormap.from_list('custom_cmap', ['black', 'green'], N=2)
+            ndvi = ndvi_masked
+            
+        if verbose:
+            plt.figure(figsize=(10, 10))
+            plt.imshow(ndvi, cmap=cmap, interpolation='none')
+            plt.colorbar(label='NDVI')
+            plt.title('Normalized Difference Vegetation Index (NDVI)')
+            plt.axis('off')
+            plt.show()
+        
+        return ndvi
+
+    def visualize_dnbr(self, dnbr_values):
+        """
+        Visualizes dNBR values using a color map.
+        
+        Parameters:
+        - dnbr_values (numpy.ndarray): dNBR values to visualize.
+        """
+        plt.figure(figsize=(10, 8))
+        plt.imshow(dnbr_values, cmap='RdYlBu_r', vmin=-1, vmax=1)
+        plt.colorbar(label='dNBR')
+        plt.title('dNBR Visualization')
+        plt.xlabel('Distance (Pixels)')
+        plt.ylabel('Distance (Pixels)')
+        #plot.show(nbr, cmap='RdYlGn', vmin=-1, vmax=1)
+        plt.show()
+
+
+
+    def visualize_image_difference(self, image1, image2):
+        diff = image1 - image2
+        diff_mask = (diff != 0)
+        colored_diff = np.zeros_like(image1, dtype=np.uint8)
+        colored_diff[diff_mask] = 255
+        
+        colored_diff_rgb = np.stack([colored_diff, np.zeros_like(colored_diff), np.zeros_like(colored_diff)], axis=-1)
+        
+        plt.figure(figsize=(10, 4))
+        plt.subplot(1, 3, 1)
+        plt.imshow(image1, cmap='gray')
+        plt.title('Image 1')
+        plt.axis('off')
+        
+        plt.subplot(1, 3, 2)
+        plt.imshow(image2, cmap='gray')
+        plt.title('Image 2')
+        plt.axis('off')
+        
+        plt.subplot(1, 3, 3)
+        plt.imshow(colored_diff_rgb)
+        plt.title('Difference (Red)')
+        plt.axis('off')
+        
+        plt.tight_layout()
+        plt.show()
+
+    def calculate_and_plot_nbr(self,src_dataset, threshold=None):
+        """
+        Sentinel-2 görüntüsünden Normalized Burn Ratio (NBR) hesaplar ve görselleştirir.
+        
+        Args:
+        src_dataset (object): Sentinel-2 TIFF dosyası veri kümesi.
+        threshold (float or None): NDVI için eşik değeri. None ise eşik uygulanmaz.
+        """
+        # NIR ve SWIR bantlarını yükleyin
+        nir_band = src_dataset.read(8).astype(np.float32)
+        swir_band = src_dataset.read(11).astype(np.float32)
+    
+        # NIR ve SWIR bantlarının değerleri sıfırdan büyük olan yerlerde NBR hesaplayın
+        nbr = np.zeros_like(nir_band, dtype=np.float32)
+        valid_mask = np.logical_and(nir_band > 0, swir_band > 0)
+        nbr[valid_mask] = (nir_band[valid_mask] - swir_band[valid_mask]) / (nir_band[valid_mask] + swir_band[valid_mask])
+    
+        # Eğer threshold belirtilmişse, NBR'yi bu eşik değerine göre maskeleyin
+        if threshold is not None:
+            nbr_masked = np.zeros_like(nbr, dtype=np.uint8)
+            nbr_masked[nbr >= threshold] = 1
+            nbr_masked[nbr < threshold] = 0
+            nbr = nbr_masked.astype(np.float32)  # Maskelenmiş NBR'yi kullan
+    
+            # Özel renk skalası için lineer segmentli bir renk haritası oluşturun
+            cmap = LinearSegmentedColormap.from_list('custom_cmap', ['red', 'green'], N=2)
+    
+        else:
+            cmap = 'RdYlGn'  # Eğer eşik belirtilmemişse varsayılan renk haritası kullan
+    
+        # Renkli görselleştirme ve renk skalası eklemek için matplotlib kullanın
+        plt.figure(figsize=(10, 8))
+        plt.imshow(nbr, cmap=cmap)
+        plt.colorbar(label='NBR Masked' if threshold is not None else 'NBR')
+        plt.title('Normalized Burn Ratio (NBR)')
+        plt.show()
--- rasterhandler.py
+++ rasterhandler.py
@@ -2,41 +2,62 @@ import rasterio
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import enum
+
+class AreaType(enum.Enum):
+    BURNED = 1
+    UNBURNED = 2
 
 class RasterHandler:
-    def __init__(self, tiff_path):
-        self.__src = rasterio.open(tiff_path)
-        self.__metadata = self.src.meta
-    @property
-    def src(self):
-        return self.__src
-    def get_metadata(self):
-        df_metadata = pd.DataFrame(self.__metadata.items(), columns=['Meta Veri', 'Değer'])
-        meta_aciklamalari = {
-            'driver': 'Kullanılan sürücü veya veri formatı',
-            'dtype': 'Veri tipi (örneğin, uint16)',
-            'nodata': 'Geçersiz (boş) değerlerin tanımlanması',
-            'width': 'Görüntünün genişliği piksel cinsinden',
-            'height': 'Görüntünün yüksekliği piksel cinsinden',
-            'count': 'Bant sayısı',
-            'crs': 'Koordinat referans sistemi (EPSG kodu ile belirtilen)',
-            'transform': 'Piksel koordinatlarının dünya koordinatlarına dönüşüm matrisi'
+    def __init__(self, burned_tiff_path, unburned_tiff_path):
+        self._src = {
+            AreaType.BURNED: rasterio.open(burned_tiff_path),
+            AreaType.UNBURNED: rasterio.open(unburned_tiff_path)
         }
-        df_metadata['Açıklama'] = df_metadata['Meta Veri'].map(meta_aciklamalari)
+    def src(self,area_type):
+            return self._src[area_type]
+    def _validate_area_type(self, area_type):
+        if area_type not in AreaType:
+            raise ValueError("Invalid area type specified.")
+
+    def get_metadata(self, area_type):
+        self._validate_area_type(area_type)
+        
+        src = self._src[area_type]
+        meta_data = src.meta
+        meta_descriptions = {
+            'driver': 'Driver used or data format',
+            'dtype': 'Data type (e.g., uint16)',
+            'nodata': 'Definition of invalid (empty) values',
+            'width': 'Image width in pixels',
+            'height': 'Image height in pixels',
+            'count': 'Number of bands',
+            'crs': 'Coordinate reference system (specified with EPSG code)',
+            'transform': 'Transformation matrix from pixel coordinates to world coordinates'
+        }
+        df_metadata = pd.DataFrame(meta_data.items(), columns=['Metadata', 'Value'])
+        df_metadata['Description'] = df_metadata['Metadata'].map(meta_descriptions)
         print(df_metadata.head(5))
-    
-    def save_bands(self):
-        for band_idx in range(1, self.__src.count + 1):
-            band = self.__src.read(band_idx)
+
+    def _save_band(self, area_type, band_idx, output_path, band):
+        with rasterio.open(output_path, 'w', driver='GTiff', width=self._src[area_type].width, height=self._src[area_type].height, count=1, dtype=band.dtype) as dst:
+            dst.write(band, 1)
+            print(f"Band {band_idx} saved: {output_path}")
+
+    def save_bands(self, area_type):
+        self._validate_area_type(area_type)
+
+        for band_idx in range(1, self._src[area_type].count + 1):
+            band = self._src[area_type].read(band_idx)
             output_path = f"outputs/band{band_idx}.tif"
-            with rasterio.open(output_path, 'w', driver='GTiff', width=self.__src.width, height=self.__src.height, count=1, dtype=band.dtype) as dst:
-                dst.write(band, 1)
-                print(f"Band {band_idx} kaydedildi: {output_path}")
-    
-    def visualize_rgb(self):
-        red = self.__src.read(4)   # Bant 4
-        green = self.__src.read(3) # Bant 3
-        blue = self.__src.read(2)  # Bant 2
+            self._save_band(area_type, band_idx, output_path, band)
+
+    def visualize_rgb(self, area_type):
+        self._validate_area_type(area_type)
+
+        red = self._src[area_type].read(4)   # Band 4
+        green = self._src[area_type].read(3) # Band 3
+        blue = self._src[area_type].read(2)  # Band 2
 
         rgb_image = np.stack([red, green, blue], axis=-1)
         rgb_image = rgb_image / np.max(rgb_image)
@@ -44,21 +65,26 @@ class RasterHandler:
         plt.figure(figsize=(8, 6))
         plt.imshow(rgb_image)
         plt.axis('off')
-        plt.title('Sentinel-2 RGB Görüntü')
+        plt.title('Sentinel-2 RGB Image')
         plt.show()
 
         output_path = "./outputs/sentinel2_rgb.png"
         plt.imsave(output_path, rgb_image)
-        print(f"RGB görüntü başarıyla kaydedildi: {output_path}")
+        print(f"RGB image saved successfully: {output_path}")
 
-    def save_bands_with_colormap(self):
-        for band_idx in range(1, self.__src.count + 1):
-            band = self.__src.read(band_idx)
-            cmap = plt.cm.get_cmap('viridis')  # Renk skalası (viridis örneği)
-            normed_band = (band - band.min()) / (band.max() - band.min())  # Normalizasyon
-            band_colored = (cmap(normed_band) * 255).astype(np.uint8)  # 0-255 arasına dönüştürme ve uint8 türüne dönüştürme
+    def _save_band_with_colormap(self, area_type, band_idx, band):
+        cmap = plt.cm.get_cmap('viridis')  # Color map (example: viridis)
+        normed_band = (band - band.min()) / (band.max() - band.min())  # Normalization
+        band_colored = (cmap(normed_band) * 255).astype(np.uint8)  # Convert to 0-255 and to uint8 type
 
-            output_path = f"outputs/band{band_idx}_colored.tif"
-            with rasterio.open(output_path, 'w', driver='GTiff', width=self.__src.width, height=self.__src.height, count=4, dtype=band_colored.dtype) as dst:
-                dst.write(band_colored.transpose(2, 0, 1), indexes=[1, 2, 3, 4])
-                print(f"Band {band_idx} renk skalası ile kaydedildi: {output_path}")
+        output_path = f"outputs/band{band_idx}_colored.tif"
+        with rasterio.open(output_path, 'w', driver='GTiff', width=self._src[area_type].width, height=self._src[area_type].height, count=4, dtype=band_colored.dtype) as dst:
+            dst.write(band_colored.transpose(2, 0, 1), indexes=[1, 2, 3, 4])
+            print(f"Band {band_idx} saved with colormap: {output_path}")
+
+    def save_bands_with_colormap(self, area_type):
+        self._validate_area_type(area_type)
+
+        for band_idx in range(1, self._src[area_type].count + 1):
+            band = self._src[area_type].read(band_idx)
+            self._save_band_with_colormap(area_type, band_idx, band)
